1c8cb6c1f8089508ab565f0224cdfb986c17e1a9
daniloss170828-coder <daniloss170828@gmail.com>
2025-11-06 23:09:37 -0300

    Add files via upload

---
+++ Csharp/Imc.cs
@@ -0,0 +1,15 @@
+Imc:
+using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Peso (kg): ");
+        double peso = double.Parse(Console.ReadLine());
+        Console.Write("Altura (m): ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double imc = peso / (altura * altura);
+
+        Console.WriteLine($"IMC = {imc:F2}");
+    }
+}
